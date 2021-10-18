9edcbc4709e53cfed3a6d1493926c2178b474038
Richard Perdomo <richard.perdomo121@gmail.com>
2021-10-18 15:24:10 +0200

    budget working, added functionability to add or substract money from the budget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [totalPrice, setTotalPrice] = useState(0)
+
+  const addToTotal = (amount, e) => {
+    if (e.target.checked) {
+      setTotalPrice((prev) => prev + amount)
+    } else {
+      setTotalPrice((prev) => prev - amount)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>What you want to do?</p>
+      <label htmlFor="web">Web Page (500e)</label>
+      <input type="checkbox" name="web" onChange={(e) => addToTotal(500, e)} />
+      <label htmlFor="seo">SEO consulting (300e)</label>
+      <input type="checkbox" name="seo" onChange={(e) => addToTotal(300, e)} />
+      <label htmlFor="ads"> Google ads campaign (200e)</label>
+      <input type="checkbox" name="ads" onChange={(e) => addToTotal(200, e)} />
+
+      <h2>Total:{totalPrice} </h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
