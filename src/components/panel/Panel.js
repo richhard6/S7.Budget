6e7276cb0437a298b05b60b050564c23886d9bc0
Richard Perdomo <richard.perdomo121@gmail.com>
2021-10-19 16:01:48 +0200

    bug fixed, can add and remove the extras without the last bug but now its removing each time you erase from the input

--- src/components/panel/Panel.js
+++ src/components/panel/Panel.js
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react'
 
 function Panel({ setTotalPrice, isClicked }) {
   const [adds, setAdds] = useState({ pages: 0, languages: 0 })
+  const [lastAdd, setLastAdd] = useState(0)
 
   useEffect(() => {
-    setTotalPrice(
-      (prevPrice) => (prevPrice = prevPrice + adds.pages * adds.languages * 30)
-    )
+    const add = adds.pages * adds.languages * 30
+    setTotalPrice((prevPrice) => (prevPrice = prevPrice + add))
+
+    setLastAdd((prev) => (prev = add))
   }, [adds, setTotalPrice])
 
   useEffect(() => {
@@ -14,27 +16,34 @@ function Panel({ setTotalPrice, isClicked }) {
   }, [isClicked])
 
   const getAdds = (type, e) => {
-    switch (type) {
-      case 'pages':
-        setAdds((prevAdd) => {
-          return {
-            ...prevAdd,
-            pages: parseInt(e.target.value),
-          }
-        })
+    if (e.target.value) {
+      switch (type) {
+        case 'pages':
+          if (e.target.value.length > 0)
+            setAdds((prevAdd) => {
+              return {
+                ...prevAdd,
+                pages: parseInt(e.target.value),
+              }
+            })
 
-        break
-      case 'languages':
-        setAdds((prevAdd) => {
-          return {
-            ...prevAdd,
-            languages: parseInt(e.target.value),
-          }
-        })
-        break
+          break
+        case 'languages':
+          if (e.target.value.length > 0)
+            setAdds((prevAdd) => {
+              return {
+                ...prevAdd,
+                languages: parseInt(e.target.value),
+              }
+            })
+          break
 
-      default:
-        break
+        default:
+          break
+      }
+    } else {
+      setTotalPrice((prev) => prev - lastAdd)
+      setLastAdd((prev) => (prev = 0))
     }
   }
 
