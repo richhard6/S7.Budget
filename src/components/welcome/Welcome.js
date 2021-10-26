a39bd7766e867940e1fe61e2c26effa18c7e318a
Richard Perdomo <richard.perdomo121@gmail.com>
2021-10-26 18:25:26 +0200

    Welcome component created with its styled components, need to route it with reactrouter, dependency already installed

---
+++ src/components/welcome/Welcome.js
@@ -0,0 +1,13 @@
+import { Start, Title } from './styles'
+
+function Welcome() {
+  return (
+    <>
+      <Title>Welcome to CreateYourBudget</Title>
+
+      <Start>START</Start>
+    </>
+  )
+}
+
+export default Welcome
---
+++ src/components/welcome/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+  font-weight: 900;
+`
+
+export const Start = styled.button`
+  color: white;
+  cursor: pointer;
+  font-size: 2.5rem;
+  border: none;
+  background-color: orange;
+  padding: 1.5rem;
+`
