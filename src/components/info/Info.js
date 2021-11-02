c6f6c2a2668491aa7a98fcd3331c4098b1d18d2e
Richard Perdomo <richard.perdomo121@gmail.com>
2021-11-02 12:55:25 +0100

    modal closing when clicking outside. info component

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -1,26 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import Modal from '../modal/Modal'
 
 function Info({ text }) {
+  const ref = useRef()
   const [showModal, setShowModal] = useState(false)
 
   const handleClick = () => {
     setShowModal(true)
   }
-  //hay quje hacer que se cierre caiundo clicamps afuera, no solo la X del boton..... y tambien crear styled comPOnents para el text.
+
   const handleClose = () => {
     setShowModal(false)
   }
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (showModal && ref.current && !ref.current.contains(e.target)) {
+        setShowModal(false)
+      }
+    }
+
+    document.addEventListener('mousedown', checkIfClickedOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', checkIfClickedOutside)
+    }
+  }, [showModal])
+
   return (
-    <>
+    <div ref={ref}>
       <button onClick={handleClick}>i</button>
       {showModal && (
         <Modal onClose={handleClose}>
           <p>{text}</p>
         </Modal>
       )}
-    </>
+    </div>
   )
 }
 
