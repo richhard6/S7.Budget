bdb0e80a64cccbddd0039a76a8fd9d8935ae7ff7
Richard Perdomo <richard.perdomo121@gmail.com>
2021-11-02 13:34:07 +0100

    created budgetList Component .. wrote the logic and the  bugs related to it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Main from './components/main/Main'
 import Welcome from './components/welcome/Welcome'
 function App() {
   return (
     <Router>
-      {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
       <Switch>
         <Route path="/budget">
           <Main />
---
+++ src/components/budgetList/BudgetList.js
@@ -0,0 +1,8 @@
+function BudgetList() {
+  //aqui recogeremos del local storage todos los presupuestos previamente guardados(recordar que hay unbug en el precio.)
+  //guardado en LocalStorage.  entonces se ordenan alfabeticamente y  por fecha de creacion y reiniciar el orden
+  //el state de Budget debe tener ahora un TIEMPO de creacion......... Y creare un boton para guardar en localStorage, asi no se guarda con cada Onchjange
+  return <div>This is the budgetList</div>
+}
+
+export default BudgetList
