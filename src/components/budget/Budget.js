f329b111a0daf747e5c7bb165597cb6eaff0c817
Richard Perdomo <richard.perdomo121@gmail.com>
2021-11-10 15:57:02 +0100

    budgets adding accordingly updating which each save, must change the save from location and sort options, and searchbar

--- src/components/budget/Budget.js
+++ src/components/budget/Budget.js
@@ -1,6 +1,10 @@
 import { TableData, TableWrapper } from '../budgetList/styles'
-
-function Budget({ budget }) {
+import { useEffect } from 'react'
+function Budget({ budget, allStorage }) {
+  useEffect(() => {
+    allStorage()
+    // aqui uando le damos a SAVE no se esta actualizaxndo la lista de budgets
+  }, [budget, allStorage])
   return (
     <TableWrapper>
       <TableData> {budget.budgetName}</TableData>
--- src/components/budgetList/BudgetList.js
+++ src/components/budgetList/BudgetList.js
@@ -1,17 +1,19 @@
 import Budget from '../budget/Budget'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 
 import { Table, TableBody, TableHeading, TableRow } from './styles'
 import { Button, WrapperButton } from '../extra/styles'
 
 function BudgetList({ budget }) {
-  //Hay que crear enn este componente un botton para organizarlo
-  //alfabeticamente y por fecha de creacion
-  //un buton para poder reinciar el orden,
   //componente separado para search bar, que mostrara solo entonces
   //el nombre de budget que coincida con el user  input
 
   const [allBudgets, setAllBudgets] = useState([])
+
+  useEffect(() => {
+    allStorage()
+    // aqui uando le damos a SAVE no se esta actualizaxndo la lista de budgets
+  }, [budget])
 
   const allStorage = () => {
     var values = [],
@@ -25,15 +27,13 @@ function BudgetList({ budget }) {
     setAllBudgets((prevBudgets) => (prevBudgets = values))
   }
 
-  useEffect(() => {
-    allStorage()
-    // aqui uando le damos a SAVE no se esta actualizaxndo la lista de budgets
-  }, [budget])
-
   //crear que budgetList se muestre condicionalmente solo si hay budgets en el local... eso se haria en el componente padre
+
+  //depende del budget que se estd creando, antes de darle a save, la URL debe cambiar y mostrar los modificadores correspondientes.
   return (
     <>
       <WrapperButton>
+        s0rt
         <Button>alfabetica</Button>
         <Button>createdAt</Button>
         <Button>reiniciar orden</Button>
@@ -51,7 +51,7 @@ function BudgetList({ budget }) {
               <Budget
                 key={index}
                 budget={JSON.parse(budget)}
-                update={allStorage}
+                allStorage={allStorage}
               />
             )
           })}
